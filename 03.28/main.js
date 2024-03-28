4e92647d00a8ef4de4cfa0d36e587d727e49f6fd
“[ilvintagiyev]” <[email>
2024-03-28 19:06:45 +0400

    init

---
+++ 03.28/main.js
@@ -0,0 +1,142 @@
+// task1
+// let a;
+// a = +window.prompt("yasi daxil edin");
+// if( a < 125){
+//     if( a>=65){
+//         console.log("pensiya dusecek")
+//     }
+//     else{
+//         console.log("hele cavansan")
+     
+// }    
+// }
+// else{
+//     console.log("olmusen")
+// }
+// task2
+// let a;
+// a = +window.prompt("ededi daxil edin");
+// if(a%2==0){
+//     console.log("cut ededdir")
+// }
+// else{
+//     console.log("tek ededdir")
+// }
+// task3
+// let month;
+// month = window.prompt("ayi girin");
+// switch (month) {
+//     case "dekabr":
+//     case "yanvar":
+//     case "fevral":
+//         console.log("winter");
+//         break;
+//     case "mart":
+//     case "aprel":
+//     case "may":
+//         console.log("spring");
+//         break;
+//     case "iyun":
+//     case "iyul":
+//     case "avqust":
+//         console.log("summer");
+//     case "iyun":
+//     case "iyul":
+//     case "avqust":
+//         console.log("autumn");
+//         break;
+//         default:
+//           console.log("BU AY YOXDUR")
+//           break
+// }
+// task4
+// let a;
+// a = +window.prompt("ededi  daxil edin")
+// if(a % 3 == 0 && a % 5 == 0){
+//     console.log("3 ve 5 e tam bolunur")
+// }
+// else{
+//     console.log("3 ve 5e tam bolunmur")
+// }
+// task5
+// let a = +window.prompt("enter number")
+// if(a>0){
+//     if(a%2 == 0){
+//         console.log("bolundu")
+//     }
+//     else{
+//         console.log(a%2)
+
+//     }
+// }
+// task6
+// let a= +window.prompt("1 ci ededi daxil edin");
+// let b= +window.prompt("2 ci ededi daxil edin");
+// if(a*b>0){
+//     console.log("musbet");
+// }
+// else{
+//     console.log("menfi")
+// }
+// task7
+// let a= +window.prompt("ededi daxil edin");
+// if(a>0)
+// {
+//     if(a>0 && a<100){
+//         console.log("1-100 araliqindadir")
+
+//     }
+//     else{
+//         console.log("1-100 araliqinda deyil")
+        
+//     }
+    
+// }
+// else{
+//     console.log("eded menfidir")
+// }
+// task8
+// let a= +window.prompt("a ededini daxil edin");
+// let b= +window.prompt("b ededini daxil edin");
+// let c= +window.prompt("c ededini daxil edin");
+// if(a>b && a>c){
+//     console.log("a en boyukdur")
+
+// }
+// else if(b>a && b>c){
+//     console.log(" b en boyukdur")
+// }
+// else{
+//     console.log("c en boyukdur")
+// }
+
+// task10
+// let a= +window.prompt("ededi daxil edin");
+// if(a>=90){
+//     console.log("a");
+
+// }
+// else if(a>=80){
+//     console.log("b");
+// }
+// else if(a>=70){
+//     console.log("c");
+// }
+// else if(a>=60){
+//     console.log("d");
+// }
+// else if(a>=50){
+//     console.log("e");
+// }
+// else{
+//     console.log("f")
+// }
+// task
+
+
+
+
+
+
+
+
