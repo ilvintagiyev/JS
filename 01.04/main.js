36fd335787bc5404ef80ad53c3a1051a91316987
“[ilvintagiyev]” <[email>
2024-04-01 19:34:56 +0400

    push

---
+++ 01.04/main.js
@@ -0,0 +1,54 @@
+// let a = "alma";
+// let b = "salam"
+
+// const getCommonCharCount = (str1,str2)=> {
+//       let arr = str1.split{};//[a,b,c,d]
+//       let res = [];
+//       for (let i = 0; i < str2.length; i++) {
+//           if(arr.includes(str2[i])){
+//             // console.log(str2[i]);
+//             res.push(str2[i]);
+
+//         }
+        
+//       }
+//       return [...new Set(res)];
+// };
+
+// console.log("bf204".split(""));
+// TASK1
+// function volumeOfBox(size){
+//     return size.width*size.length*size.height;
+// } 
+// console.log(volumeOfBox({width:2 , length:5  ,  height:1}));
+
+// console.log(volumeOfBox({width:4 , length:7  ,  height:2}));
+
+// console.log(volumeOfBox({width:2 , length:8  ,  height:3}));
+// TASK2
+// function getCaptalizeWord(str){
+//     str=str.split(" ");
+
+//     for (let i = 0; i < str.length; i++) {
+//         str[i]=str[i][0].toUpperCase() +str[i].slice(1)
+//     }
+//     return str.join(" ")
+// }
+// console.log(getCaptalizeWord("Salam bu gun ders agir kecir"));
+// TASK3
+// function getMaxLengthWord(sentence){
+//     sentence=sentence.split(" ")
+//     let max=0
+//     let str=""
+//     for (let i = 0; i < sentence.length; i++) {
+//         if(sentence[i].length>max){
+//             max=sentence[i].length
+//             str=sentence[i]
+//         }
+        
+//     }
+//     return str;
+// }
+// console.log(getMaxLengthWord("salam necesen veziyyet"));
+// TASK4
+
